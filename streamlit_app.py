8ad808e7538256c61edce0c277bf7e6b3718c500
Dn-iel <danielsnow229@gmail.com>
2025-03-17 14:16:39 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import joblib
 
-st.title('🎈 App Name')
+def main():
+  st.title('🎈 Dermatology Machine Learning')
+  st.write('This app using machine learning')
 
-st.write('Hello world!')
+  # input by user
+  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
+
+
+if_name_=="_main":
+  main()
